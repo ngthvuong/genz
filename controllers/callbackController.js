8a74d264225876af88eb354a22a06e21a3090305
Vuong Nguyen <ngthvuong@gmail.com>
2024-10-07 00:25:01 +0700

    test for fun

--- controllers/callbackController.js
+++ controllers/callbackController.js
@@ -3,8 +3,8 @@
 const controller = {}
 
 controller.zaloPay = async (req, res) => {
-    console.log(process.env.ENCRYPTION_KEY)
-    res.render('home')
+    console.log(req.query)
+    return res.send("ban đã đóng góp thành công")
 }
 
 module.exports = controller
