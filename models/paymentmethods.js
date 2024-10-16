4630ffa3b6f2ecd38fb65a360e458a943107786b
raito1212 <ninhvo2020@gmail.com>
2024-10-16 22:59:17 +0700

    update paymentmethods

---
+++ models/paymentmethods.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class PaymentMethods extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  PaymentMethods.init({
+    code: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM('online', 'offline'),
+      allowNull: false,
+    }
+
+  }, {
+    sequelize,
+    modelName: 'PaymentMethods',
+  });
+  return PaymentMethods;
+};
