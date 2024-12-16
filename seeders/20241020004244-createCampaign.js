54070177f94184593db7a7bfa19564f7ddb92383
Vuong Nguyen <ngthvuong@gmail.com>
2024-12-16 12:26:38 +0700

    fixed campaign seeder

--- seeders/20241020004244-createCampaign.js
+++ seeders/20241020004244-createCampaign.js
@@ -31,9 +31,9 @@ module.exports = {
     charities.forEach(charity => (
       campaigns.push(
         {
-          name: "Chiến Dịch Cứu Trợ Miền Trung 1",
-          startDate: new Date('2024-07-14 00:00:00'),
-          endDate: new Date('2024-09-14 00:00:00'),
+          name: "Chiến Dịch Cứu Trợ Miền Trung "+ charity.id +"1",
+          startDate: new Date('2024-08-14 00:00:00'),
+          endDate: new Date('2025-01-14 00:00:00'),
           goal: "Mục tiêu cứu trợ 1",
           description: "Sáng 11-9, phóng viên Tuổi Trẻ đã có mặt tại hiện trường vụ lũ quét kinh hoàng ở thôn Làng Nủ, xã Phúc Khánh, huyện Bảo Yên, tỉnh Lào Cai. Tang thương bao trùm khi thảm họa này đã xóa sổ một thôn với hơn 37 hộ dân.\nThông tin chính thức đến 11h ngày 11-9 về thiệt hại do lũ quét tại Làng Nủ đã vùi lấp 37 hộ dân với 158 người. Số thi thể được tìm thấy trong đống đổ nát ngày một tăng, đã có 30 người chết.\nTheo chính quyền huyện Bảo Yên, hiện vẫn còn 70 người mất tích trong cơn lũ quét, trong đó có 18 trẻ dưới 6 tuổi, 14 trẻ dưới 14 tuổi. Trong cơn lũ quét kinh hoàng có 46 người may mắn chạy thoát.\nSáng 11-9, lực lượng quân đội, công an vẫn đang gấp rút đào bới đống đổ nát để tìm kiếm những người dân của thôn Làng Nủ bị vùi lấp trong cơn lũ quét kinh hoàng.\nCảnh tượng hãi hùng đập vào mắt lực lượng cứu hộ khi đến trưa nay đã có 25 thi thể được tìm thấy. Vẫn còn 70 người dân của thôn đang mất tích, trong đó có nhiều trẻ nhỏ dưới 6 tuổi.",
           budget: 5000000000,
@@ -42,9 +42,9 @@ module.exports = {
           charityID: charity.id,
         },
         {
-          name: "Chiến Dịch Cứu Trợ Miền Trung 2",
-          startDate: new Date('2024-08-14 00:00:00'),
-          endDate: new Date('2024-10-14 00:00:00'),
+          name: "Chiến Dịch Cứu Trợ Miền Trung "+ charity.id +"2",
+          startDate: new Date('2024-09-14 00:00:00'),
+          endDate: new Date('2025-11-14 00:00:00'),
           goal: "Mục tiêu cứu trợ 2",
           description: "Sáng 11-9, phóng viên Tuổi Trẻ đã có mặt tyại hiện trường vụ lũ quét kinh hoàng ở thôn Làng Nủ, xã Phúc Khánh, huyện Bảo Yên, tỉnh Lào Cai. Tang thương bao trùm khi thảm họa này đã xóa sổ một thôn với hơn 37 hộ dân.\nThông tin chính thức đến 11h ngày 11-9 về thiệt hại do lũ quét tại Làng Nủ đã vùi lấp 37 hộ dân với 158 người. Số thi thể được tìm thấy trong đống đổ nát ngày một tăng, đã có 30 người chết.\nTheo chính quyền huyện Bảo Yên, hiện vẫn còn 70 người mất tích trong cơn lũ quét, trong đó có 18 trẻ dưới 6 tuổi, 14 trẻ dưới 14 tuổi. Trong cơn lũ quét kinh hoàng có 46 người may mắn chạy thoát.\nSáng 11-9, lực lượng quân đội, công an vẫn đang gấp rút đào bới đống đổ nát để tìm kiếm những người dân của thôn Làng Nủ bị vùi lấp trong cơn lũ quét kinh hoàng.\nCảnh tượng hãi hùng đập vào mắt lực lượng cứu hộ khi đến trưa nay đã có 25 thi thể được tìm thấy. Vẫn còn 70 người dân của thôn đang mất tích, trong đó có nhiều trẻ nhỏ dưới 6 tuổi.",
           budget: 5000000000,
@@ -53,9 +53,9 @@ module.exports = {
           charityID: charity.id,
         },
         {
-          name: "Chiến Dịch Cứu Trợ Miền Trung 3",
-          startDate: new Date('2024-09-14 00:00:00'),
-          endDate: new Date('2024-11-14 00:00:00'),
+          name: "Chiến Dịch Cứu Trợ Miền Trung "+ charity.id +"3",
+          startDate: new Date('2025-01-14 00:00:00'),
+          endDate: new Date('2025-12-14 00:00:00'),
           description: "Sáng 11-9, phóng viên Tuổi Trẻ đã có mặt tại hiện trường vụ lũ quét kinh hoàng ở thôn Làng Nủ, xã Phúc Khánh, huyện Bảo Yên, tỉnh Lào Cai. Tang thương bao trùm khi thảm họa này đã xóa sổ một thôn với hơn 37 hộ dân.\nThông tin chính thức đến 11h ngày 11-9 về thiệt hại do lũ quét tại Làng Nủ đã vùi lấp 37 hộ dân với 158 người. Số thi thể được tìm thấy trong đống đổ nát ngày một tăng, đã có 30 người chết.\nTheo chính quyền huyện Bảo Yên, hiện vẫn còn 70 người mất tích trong cơn lũ quét, trong đó có 18 trẻ dưới 6 tuổi, 14 trẻ dưới 14 tuổi. Trong cơn lũ quét kinh hoàng có 46 người may mắn chạy thoát.\nSáng 11-9, lực lượng quân đội, công an vẫn đang gấp rút đào bới đống đổ nát để tìm kiếm những người dân của thôn Làng Nủ bị vùi lấp trong cơn lũ quét kinh hoàng.\nCảnh tượng hãi hùng đập vào mắt lực lượng cứu hộ khi đến trưa nay đã có 25 thi thể được tìm thấy. Vẫn còn 70 người dân của thôn đang mất tích, trong đó có nhiều trẻ nhỏ dưới 6 tuổi.",
           goal: "Mục tiêu cứu trợ 3",
           budget: 5000000000,
@@ -64,9 +64,9 @@ module.exports = {
           charityID: charity.id,
         },
         {
-          name: "Chiến Dịch Cứu Trợ Miền Trung 4",
-          startDate: new Date('2024-11-14 00:00:00'),
-          endDate: new Date('2024-12-14 00:00:00'),
+          name: "Chiến Dịch Cứu Trợ Miền Trung "+ charity.id +"4",
+          startDate: new Date('2025-12-14 00:00:00'),
+          endDate: new Date('2025-01-14 00:00:00'),
           goal: "Mục tiêu cứu trợ 4",
           description: "Sáng 11-9, phóng viên Tuổi Trẻ đã có mặt tại hiện trường vụ lũ quét kinh hoàng ở thôn Làng Nủ, xã Phúc Khánh, huyện Bảo Yên, tỉnh Lào Cai. Tang thương bao trùm khi thảm họa này đã xóa sổ một thôn với hơn 37 hộ dân.\nThông tin chính thức đến 11h ngày 11-9 về thiệt hại do lũ quét tại Làng Nủ đã vùi lấp 37 hộ dân với 158 người. Số thi thể được tìm thấy trong đống đổ nát ngày một tăng, đã có 30 người chết.\nTheo chính quyền huyện Bảo Yên, hiện vẫn còn 70 người mất tích trong cơn lũ quét, trong đó có 18 trẻ dưới 6 tuổi, 14 trẻ dưới 14 tuổi. Trong cơn lũ quét kinh hoàng có 46 người may mắn chạy thoát.\nSáng 11-9, lực lượng quân đội, công an vẫn đang gấp rút đào bới đống đổ nát để tìm kiếm những người dân của thôn Làng Nủ bị vùi lấp trong cơn lũ quét kinh hoàng.\nCảnh tượng hãi hùng đập vào mắt lực lượng cứu hộ khi đến trưa nay đã có 25 thi thể được tìm thấy. Vẫn còn 70 người dân của thôn đang mất tích, trong đó có nhiều trẻ nhỏ dưới 6 tuổi.",
           budget: 5000000000,
